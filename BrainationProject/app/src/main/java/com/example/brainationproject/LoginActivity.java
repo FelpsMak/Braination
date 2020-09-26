aee4ab7f59fb5c03e4e3f7ed06c61340c59152a6
FelpsMak <felipemakloufc@gmail.com>
2020-09-26 18:46:16 -0400

    firebase login

--- BrainationProject/app/src/main/java/com/example/brainationproject/LoginActivity.java
+++ BrainationProject/app/src/main/java/com/example/brainationproject/LoginActivity.java
@@ -1,26 +1,48 @@
 package com.example.brainationproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import java.util.List;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    EditText cxemail;
+    EditText cxpass;
     Button btnLogin;
     TextView txtRegister;
     ImageView imgVoltar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        mAuth = FirebaseAuth.getInstance();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
         imgVoltar=findViewById(R.id.image_login_voltar);
+        cxemail=findViewById(R.id.email_text_login);
+        cxpass=findViewById(R.id.pass_text_login);
+
+
+
         imgVoltar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -32,7 +54,18 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, ListaActivity.class));
+                String email, password;
+
+                email = cxemail.getText().toString();
+                password = cxpass.getText().toString();
+
+                if(!email.trim().isEmpty() && !password.trim().isEmpty()){
+                    doLogin(email, password);
+                }else{
+                    Toast.makeText(getApplicationContext(), "Preencha os Campos Vazios", Toast.LENGTH_SHORT).show();
+                }
+
+
             }
         });
         txtRegister=findViewById(R.id.forgot_password);
@@ -40,11 +73,39 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(), "Projeto em andamento", Toast.LENGTH_SHORT).show();
+
+
             }
         });
+    }
+    private void doLogin(String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            startActivity(new Intent(LoginActivity.this, ListaActivity.class));
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        // ...
+                    }
+                });
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
 
     }
-
 
     public void sussa(View view) {
         Toast.makeText(getApplicationContext(), "Projeto em andamento", Toast.LENGTH_SHORT).show();
