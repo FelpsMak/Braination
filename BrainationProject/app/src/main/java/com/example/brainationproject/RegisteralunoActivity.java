876303152af3a0b831a7b027db1724af0f05b7c8
unknown <diogojuan.pi@gmail.com>
2020-10-11 20:51:02 -0300

    ajuda lê

--- BrainationProject/app/src/main/java/com/example/brainationproject/RegisteralunoActivity.java
+++ BrainationProject/app/src/main/java/com/example/brainationproject/RegisteralunoActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -28,6 +27,11 @@ public class RegisteralunoActivity extends AppCompatActivity {
 
         String name, email, senha;
         btnRegister=findViewById(R.id.btnRegisterA);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisteralunoActivity.this,LoginActivity.class));
+            }}
         Rnome=findViewById(R.id.edit_text_nome);
         Remail=findViewById(R.id.edit_text_email);
         Rsenha=findViewById(R.id.edit_text_senha);
@@ -47,12 +51,6 @@ public class RegisteralunoActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()) {
                             FirebaseUser user = mAuth.getCurrentUser();
-                            btnRegister.setOnClickListener(new View.OnClickListener() {
-                                @Override
-                                public void onClick(View v) {
-                                    startActivity(new Intent(RegisteralunoActivity.this,LoginActivity.class));
-                                }
-                            });
                         }
                         else {
                             Toast.makeText(RegisteralunoActivity.this, "Authentication failed",Toast.LENGTH_SHORT).show();
