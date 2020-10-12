de87df772fadfb07a012d77f8af27c148f52a3c4
unknown <diogojuan.pi@gmail.com>
2020-10-11 21:55:53 -0300

    Ajustar firebase

--- BrainationProject/app/src/main/java/com/example/brainationproject/RegisteralunoActivity.java
+++ BrainationProject/app/src/main/java/com/example/brainationproject/RegisteralunoActivity.java
@@ -25,22 +25,25 @@ public class RegisteralunoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registeraluno_activity);
 
-        String name, email, senha;
+
         btnRegister=findViewById(R.id.btnRegisterA);
-        btnRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(RegisteralunoActivity.this,LoginActivity.class));
-            }}
         Rnome=findViewById(R.id.edit_text_nome);
         Remail=findViewById(R.id.edit_text_email);
         Rsenha=findViewById(R.id.edit_text_senha);
-        name=Rnome.getText().toString();
-        email=Remail.getText().toString();
-        senha=Rsenha.getText().toString();
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String name, email, senha;
+                name=Rnome.getText().toString();
+                email=Remail.getText().toString();
+                senha=Rsenha.getText().toString();
+                registerNewUser(name, email, senha);
+
+            }});
 
 
-        registerNewUser(name, email, senha);
 
     }
     private void registerNewUser(String name, String email, String password){
@@ -51,6 +54,7 @@ public class RegisteralunoActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()) {
                             FirebaseUser user = mAuth.getCurrentUser();
+                            startActivity(new Intent(RegisteralunoActivity.this,LoginActivity.class));
                         }
                         else {
                             Toast.makeText(RegisteralunoActivity.this, "Authentication failed",Toast.LENGTH_SHORT).show();
